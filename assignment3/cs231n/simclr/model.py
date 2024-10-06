544b21383017ea3c6c8b49adbf2180bc0bd275ca
wuwowuyi <dalucheng@gmail.com>
2024-10-06 13:07:43 -0700

    SimCLR input transform, simCLR model comments

--- assignment3/cs231n/simclr/model.py
+++ assignment3/cs231n/simclr/model.py
@@ -9,16 +9,26 @@ class Model(nn.Module):
         super(Model, self).__init__()
 
         self.f = []
+
+        # My Note:
+        # replace the first conv layer probably because input is CIFAR-10 rather than ImageNet images
+        # the MaxPool2d layer is removed probably for the same reason.
+        # the last project FC layer is removed because we don't do classification.
         for name, module in resnet50().named_children():
             if name == 'conv1':
                 module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
             if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
                 self.f.append(module)
+
         # encoder
         self.f = nn.Sequential(*self.f)
         # projection head
-        self.g = nn.Sequential(nn.Linear(2048, 512, bias=False), nn.BatchNorm1d(512),
-                               nn.ReLU(inplace=True), nn.Linear(512, feature_dim, bias=True))
+        self.g = nn.Sequential(
+            nn.Linear(2048, 512, bias=False),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, feature_dim, bias=True)
+        )
 
     def forward(self, x):
         x = self.f(x)
